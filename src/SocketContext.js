3ec9eff6a07e9d1f48dd765e1a47a6a27577599b
Okoli Evans <95226065+OkoliEvans@users.noreply.github.com>
2022-08-30 16:56:50 +0530

    Update SocketContext.js

--- src/SocketContext.js
+++ src/SocketContext.js
@@ -6,7 +6,7 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:5000'); 
+const socket = io('https://lexia.herokuapp.com/'); 
 
 const ContextProvider = ({ children }) => {
   const [ stream, setStream ] = useState();
